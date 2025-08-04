4c3dd93462fd4f21f08fa487f672658c52bce479
Ghada Al-Ani <ghadaabdulaziz1@gmail.com>
2025-08-04 18:09:01 +0000

    still not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,4 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 
 const firebaseConfig = {
   "projectId": "nutrition-navigator-4tqm7",
@@ -13,7 +12,11 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+let app: FirebaseApp;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
 
 export { app };
