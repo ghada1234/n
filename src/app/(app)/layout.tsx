c92cae0884022d4dd824aebae0c07118d3cd7ce4
Ghada Al-Ani <ghadaabdulaziz1@gmail.com>
2025-08-05 13:43:50 +0000

    remove extra space from screen

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -62,8 +62,8 @@ export default function AppLayout({
     <SidebarProvider>
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <AppSidebar onLinkClick={() => setIsMobileSheetOpen(false)} />
-        <div className="flex flex-col sm:gap-4 sm:py-4 lg:pl-64">
-           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <div className="flex flex-col lg:pl-64">
+           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6">
              <Sheet open={isMobileSheetOpen} onOpenChange={setIsMobileSheetOpen}>
                 <SheetTrigger asChild>
                   <Button size="icon" variant="outline" className="lg:hidden">
@@ -116,7 +116,7 @@ export default function AppLayout({
             </div>
           </header>
           <main className="flex-1">
-            <div className="p-4 lg:p-8 pt-0">{children}</div>
+            <div className="p-4">{children}</div>
           </main>
         </div>
       </div>
